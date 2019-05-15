3607925be02acc8abe51eed5408402f067a4b233
Diego <diego-vespa2020@outlook.com.br>
2019-05-15 14:36:19 -0300

    atualizando-pt2

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -9,7 +9,8 @@ import { IonSlides } from '@ionic/angular';
 export class LoginPage implements OnInit {
 
 @ViewChild(IonSlides) slides: IonSlides
-
+public wavesPosition: number=0;
+public wavesDifference: number=80;
   constructor() { }
 
   ngOnInit() {}
@@ -18,8 +19,10 @@ export class LoginPage implements OnInit {
     segmentChanged(event:any){
       if(event.detail.value === "login"){
         this.slides.slidePrev();
+        this.wavesPosition+=this.wavesDifference;
       }else{
         this.slides.slideNext();
+        this.wavesPosition-=this.wavesDifference;
       }
       
     
